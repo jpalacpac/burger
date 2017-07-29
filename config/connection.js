8cd18a8ad0397f1c75309fab4d160c034fc610fd
jpalacpac <jpalacpac07@gmail.com>
2017-07-28 23:23:52 -0700

    fixed bugs

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,6 @@
 var mysql = require("mysql");
 var connection;
 
-  connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } 
