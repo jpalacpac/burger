8d708780cd8a31540a5f8d8d126bec0ae2d5d37f
jpalacpac <jpalacpac07@gmail.com>
2017-07-28 22:32:11 -0700

    adding final files

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
+CREATE TABLE burgers (
 
--- Create the burgers_db.
--- Switch to or use the burgers_db.
--- Create a burgers table with these fields:
--- id: an auto incrementing int that serves as the primary key.
--- burger_name: a string.
--- devoured: a boolean.
--- date: a TIMESTAMP.
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+
+);
+
--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 var methodOverride = require("method-override");
-var bodyParser = require("body-parser")
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_Controller.js");
+
+app.use("/", routes);
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT " + PORT)
+});
